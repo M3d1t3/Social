89b75a295e7ef550914cf9920b9827de8de4aed2
M3d1t3 <sanchezpomares1306@gmail.com>
2023-06-02 08:22:47 +0200

    act1 2/6/23

---
+++ API/registrarUsuario.php
@@ -0,0 +1,28 @@
+<?php
+    //Realizar la conexion a la base de datos
+    include("conectar.php");
+
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $correo = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    //Volver a comprobar que el correo no existe
+    $respuesta = 0;
+
+    $consulta = "select * from usuarios where email='" . $correo . "';";
+    $resultado = $conn->query($consulta);
+    while ($fila = mysqli_fetch_array($resultado)){
+        $respuesta = 1;
+    }
+
+    if($respuesta==0){
+        //El correo no existe, lo registramos
+        $consulta = "insert into usuarios (nombre, apellido, pass, email, seguidores, seguidos, posts) values ('" . $nombre . "','" . $apellido . "','" . $pass . "','" . $correo . "',0,0,0);";
+        $conn->query($consulta);
+        //Iniciamos la sesion
+        session_start();
+        $_SESSION['correo'] = $correo;
+    }
+
+?>
