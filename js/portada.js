78f99a61d06de6097349b262e2c7b8ce475f6675
M3d1t3 <sanchezpomares1306@gmail.com>
2023-06-07 22:13:53 +0200

    act1 7/6/23

--- js/portada.js
+++ js/portada.js
@@ -46,12 +46,53 @@ function cargarSugerencias(){
         success: function(response) {
             sugerencias = response;
             console.log(sugerencias);
+            moduloSugerencias();
         },
         error: function(xhr, status, error) {
           // Manejar los errores de la solicitud AJAX aquí
           console.error(error);
         }
     });
+}
 
-      
+
+
+/*Cargar el modulo sugerencias a partir de la matriz sugerencias, coge los ultimos 15 registros y crea un div para cada uno de ellos. 
+Al final crea un boton para ir a más sugerencias*/
+function moduloSugerencias(){
+    let contenedorSugerencias = $("#bloqueSugerencias");
+
+    for (let i = 0; i < sugerencias.length; i++) {
+        var sugerencia = sugerencias[i];
+        
+        // Crear el elemento de la sugerencia
+        var sugerenciaDiv = $("<div>").addClass("sugerencia");
+
+        // Crear la imagen de la foto
+        var fotoImg = $("<img>").attr("src", sugerencia.foto).attr("alt", "Foto de perfil");
+        sugerenciaDiv.append(fotoImg);
+
+        // Crear el nombre y apellido
+        var nombreApellido = $("<p>").text(sugerencia.nombre + " " + sugerencia.apellido);
+        sugerenciaDiv.append(nombreApellido);
+
+        // Crear el botón de seguir
+        var seguirBtn = $("<button>").text("Seguir").attr("data-id", sugerencia.ID);
+        sugerenciaDiv.append(seguirBtn);
+
+        // Agregar el evento de clic al botón de seguir
+        seguirBtn.on("click", function() {
+            var usuarioId = $(this).attr("data-id");
+            seguirUsuario(usuarioId);
+        });
+
+        // Agregar la sugerencia al contenedor
+        contenedorSugerencias.append(sugerenciaDiv);
+    }
+}
+
+function seguirUsuario(usuarioId) {
+    // Aquí puedes implementar la lógica para seguir al usuario con el ID proporcionado
+    // por ejemplo, puedes realizar una solicitud AJAX al servidor para enviar la solicitud de seguimiento
+    console.log("Siguiendo al usuario con ID:", usuarioId);
 }
--- portada.php
+++ portada.php
@@ -36,7 +36,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="imagenes/marcador-de-alfiler.png" type="image/png">
     <link href="dist/output.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/portada.css">
+    <link rel="stylesheet" href="css/portada.css<?php echo '?' . date("YmdHis") ?>">
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="js/portada.js"></script>
     <link rel="stylesheet" href="node_modules/cropperjs/dist/cropper.css">
@@ -161,6 +161,7 @@
             //Carga de modulos de la portada
             moduloPerfil();
             cargarSugerencias();
+            
             
         });
     </script>
